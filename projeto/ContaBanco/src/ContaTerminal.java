eab99a804666326c57fd124cde129eec32f2b7b9
C-algaro <ti.chrystian@gmail.com>
2023-02-02 20:48:10 -0300

    ajuste conta

--- projeto/ContaBanco/src/ContaTerminal.java
+++ projeto/ContaBanco/src/ContaTerminal.java
@@ -1,26 +1,24 @@
 import java.util.Scanner;
-import java.util.Locale;
 
 public class ContaTerminal {
     public static void main(String[] args) {
-        //TODO: Conhecer e importar a classe Scanner
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
-        
-        //Exibir as mensagens para o nosso usuário
-        //Obter pela scanner os valores digitados no terminal
-        System.out.println("Por favor, digite seu nome: ");
-        String nome = scanner.next();
+        try (//Conhecer e importar a classe Scanner
+        Scanner scanner = new Scanner(System.in)) {
+            {
+                //Exibir as mensagens para o nosso usuário
+                //Obter pela scanner os valores digitados no terminal
+                System.out.println("Por favor, digite seu nome: ");
+                String nome = scanner.next();
+                System.out.println("Por favor, digite o nome da Agência: ");
+                String agencia = scanner.next();
+                System.out.println("Por favor, digite o número da conta: ");
+                int numero = scanner.nextInt();
+                System.out.println("Por favor, valor da conta: ");
+                double saldo = scanner.nextDouble();
 
-        System.out.println("Por favor, digite o nome da Agência: ");
-        String agencia = scanner.next();
-
-        System.out.println("Por favor, digite o número da conta: ");
-        int numero = scanner.nextInt();
-
-        System.out.println("Por favor, valor da conta: ");
-        double saldo = scanner.nextDouble();
-
-        //Exibir a mensagem conta criada
-        System.out.println("Olá "+nome+" obrigado por criar uma conta em nosso banco, a sua Agência é "+agencia+" conta "+numero+" e seu saldo "+saldo+" já está disponível para saque.");
+                //Exibir a mensagem conta criada
+                System.out.println("Olá "+nome+" obrigado por criar uma conta em nosso banco, a sua Agência é "+agencia+" conta "+numero+" e seu saldo "+saldo+" já está disponível para saque.");
+            }
+        }
     }
 }
