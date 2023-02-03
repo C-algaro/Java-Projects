c9bf467b7dfccbb44076a1438542369c895f61c7
C-algaro <ti.chrystian@gmail.com>
2023-02-03 17:07:55 -0300

    Exception Tratament

--- ControleFluxo/src/Contador.java
+++ ControleFluxo/src/Contador.java
@@ -16,6 +16,7 @@ public class Contador {
 		}
 		
 	}
+	
 	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
 		if(parametroUm>parametroDois)
             throw new ParametrosInvalidosException();
